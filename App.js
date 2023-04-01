c8af50b85f53736eaa15877e5b8daaa40d132093
Giovana Priscila <71566094+Gipria@users.noreply.github.com>
2023-04-01 11:03:01 -0300

    Adding login page

--- App.js
+++ App.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Home from './src/Home';
+import Login from './src/Login/login';
 
 export default function App() {
   return (
-    <Home/>
+    <Home/>,
+    <Login/>
   );
 }
