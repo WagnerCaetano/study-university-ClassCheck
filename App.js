a76155e3f3c02a0878cc72ffe8f9e48a52a83286
Giovana Priscila <71566094+Gipria@users.noreply.github.com>
2023-04-10 22:18:06 -0300

    Modificando arquivo app e com ela novos imports

--- App.js
+++ App.js
@@ -1,38 +1,73 @@
-<<<<<<< HEAD
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Home from './src/Home';
-import Login from './src/Login/login';
-=======
 import React, { useEffect } from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { SafeAreaView, StyleSheet, Text, TouchableOpacity,View, Image } from "react-native";
 import Home from "./src/Home";
-import messaging from "@react-native-firebase/messaging";
-import Toast from "react-native-toast-message";
-import toastEmitter from "./src/services/toastEmitter";
->>>>>>> a6c2221d44001ebeef2d14b550acf7320adbf479
+import LoginScreen from "./src/Login/LoginScreen";
+import MaterialIcons from 'react-native-vector-icons/MaterialIcons';
 
-export default function App() {
-  /*useEffect(() => {
-    messaging().onMessage(async (remoteMessage) => {
-      const { body, title } = remoteMessage.notification;
+// import messaging from "@react-native-firebase/messaging";
+// import Toast from "react-native-toast-message";
+// import toastEmitter from "./src/services/toastEmitter";
 
-      toastEmitter.emit({
-        title,
-        subtitle: body,
-      });
-    });
-  }, []);*/
-
+const App = () => {
   return (
-<<<<<<< HEAD
-    <Home/>,
-    <Login/>
-=======
-    <>
-      <Home />
-      {/* <Toast ref={(ref) => Toast.setRef(ref)} /> */}
-    </>
->>>>>>> a6c2221d44001ebeef2d14b550acf7320adbf479
-  );
+    <SafeAreaView style={{
+      flex:1,
+      justifyContent:'center', 
+      alignItems: 'center',
+      backgroudColor: '#fff',
+    }}>
+      <View>
+        <Text style={{fontSize:30, fontWeight: 'bold', color: '#12826f'}}>
+          ClassCheck
+        </Text>
+      </View>
+      <TouchableOpacity
+      // por alguma razao nao ta aparecendo a cor do backgroudColor
+        style={{
+          backgroudColor: '#c2f2ea',
+          padding: 20,
+          width: '90%',
+          borderRadius: 5,
+          flexDirection: 'row',
+          justifyContent: 'space-between',
+        }}>
+        <Text style={{fontWeight:'bold', fontSize:18, color:'black'}}> Cadastre</Text>
+        <MaterialIcons name="arrow-forward-ios" size={22} color={'#fff'}/>
+      </TouchableOpacity>
+    </SafeAreaView>
+  )
 }
+
+// const styles = StyleSheet({
+//   root:{
+//     flex:1,
+//     justifyContent:'center', 
+//     alignItems: 'center',
+//     backgroudColor: '#fff',
+
+//   }
+
+// });
+
+export default App;
+
+// export default function App() {
+//   /*useEffect(() => {
+//     messaging().onMessage(async (remoteMessage) => {
+//       const { body, title } = remoteMessage.notification;
+
+//       toastEmitter.emit({
+//         title,
+//         subtitle: body,
+//       });
+//     });
+//   }, []);*/
+
+//   return (
+//     <>
+//       {/* <Home /> */}
+//       {/* <Toast ref={(ref) => Toast.setRef(ref)} /> */}
+//       <LoginScreen></LoginScreen>
+//     </>
+//   );
+// }
