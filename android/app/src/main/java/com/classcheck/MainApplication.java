929b6d32c259d3d2fb3d0d5aba579f27540ceaf2
wagnercaetano <wagnercaetano01@gmail.com>
2023-04-11 23:32:04 -0300

    arrumando build android

--- android/app/src/main/java/com/classcheck/MainApplication.java
+++ android/app/src/main/java/com/classcheck/MainApplication.java
@@ -30,8 +30,8 @@ public class MainApplication extends Application implements ReactApplication {
       protected List<ReactPackage> getPackages() {
         @SuppressWarnings("UnnecessaryLocalVariable")
         List<ReactPackage> packages = new PackageList(this).getPackages();
-        // Packages that cannot be autolinked yet can be added manually here, for example:
-        // packages.add(new MyReactNativePackage());
+        List<ReactPackage> packages = new PackageList(this).getPackages();
+        packages.add(new com.swmansion.rnscreens.RNScreensPackage());
         return packages;
       }
 
