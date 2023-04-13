5317a341576b1f52c8ece600133eb208157e11bc
Giovana Priscila <71566094+Gipria@users.noreply.github.com>
2023-04-12 22:30:28 -0300

    Subindo pasta

--- App.tsx
+++ App.tsx
@@ -1,8 +1,23 @@
 import React from "react";
 import Tabs from "./src/navigation/tabs";
+// import LoginScreen from
+import {SafeAreaView,ScrollView,StatusBar,StyleSheet,Text,useColorScheme,View,} from 'react-native';
+import {Colors,DebugInstructions,Header,LearnMoreLinks,ReloadInstructions,} from 'react-native/Libraries/NewAppScreen';
+
+// type SectionProps = PropsWithChildren<{
+//   title: string;
+// }>;
+
+// function Section({children, title}: SectionProps): JSX.Element {
+//   const isDarkMode = useColorScheme() === 'dark';
+
+  // const estalogado = false;
+  // return <> estalogado ? <Tabs/> : <LoginScreen/> </>;
 
 function App() {
-  return <Tabs />;
-}
+  
+  return };
+
+
 
 export default App;
