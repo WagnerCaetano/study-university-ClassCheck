cc8a6f5cf423fe0a433cf13cabdded095e3482e9
Madu <maria.dudacorrea@gmail.com>
2023-05-04 07:04:34 -0300

    mais bolotas para marcar espaço

--- src/Components/ButtonWrapper/buttonWrapper.tsx
+++ src/Components/ButtonWrapper/buttonWrapper.tsx
@@ -2,6 +2,9 @@ import React from "react";
 import { View, TouchableOpacity, Text, Image } from "react-native";
 import styled from "styled-components/native";
 import { LinearGradient } from 'expo-linear-gradient';
+import Svg, { Circle } from 'react-native-svg';
+
+
 
 const Wrapper = styled(View)`
   flex: 1;
@@ -36,18 +39,31 @@ const ButtonWrapper = () => {
       <ButtonContainer>
         <Button style={{backgroundColor: '#17A1FA'}}>
           <ButtonText>PRESENÇA</ButtonText>
+      <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+        <Circle cx="50" cy="50" r="50" stroke="none" strokeWidth=".5" fill="violet" />
+      </Svg>
+   
 
         </Button>
         <Button style={{backgroundColor: '#EEC72E'}}>
           <ButtonText>PERFIL</ButtonText>
+          <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+        <Circle cx="50" cy="50" r="50" stroke="none" strokeWidth=".5" fill="yellow" />
+      </Svg>
         </Button>
       </ButtonContainer>
       <ButtonContainer>
       <Button style={{backgroundColor: '#7BD164'}}>
           <ButtonText>SUPORTE</ButtonText>
+          <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+        <Circle cx="50" cy="50" r="50" stroke="none" strokeWidth=".5" fill="red" />
+      </Svg>
         </Button>
         <Button style={{backgroundColor: '#F08638'}}>
           <ButtonText>CALENDÁRIO</ButtonText>
+          <Svg height="50%" width="50%" viewBox="0 0 100 100" >
+        <Circle cx="50" cy="50" r="50" stroke="none" strokeWidth=".5" fill="blue" />
+      </Svg>
         </Button>
       </ButtonContainer>
     </Wrapper>
