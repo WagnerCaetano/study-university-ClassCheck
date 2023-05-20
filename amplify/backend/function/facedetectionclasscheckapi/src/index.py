143dbd79b73e53b0a07f77f522c0239f6ba317da
wagnercaetano <wagnercaetano01@gmail.com>
2023-05-20 19:02:35 -0300

    facial recognition

--- amplify/backend/function/facedetectionclasscheckapi/src/index.py
+++ amplify/backend/function/facedetectionclasscheckapi/src/index.py
@@ -32,7 +32,7 @@ def face_comparison():
 
         user_id = request.args.get('userId')
 
-        s3_object_key = f'{user_id}.jpg'
+        s3_object_key = f'{user_id}.png'
 
         response = s3_client.get_object(
             Bucket='photos173431-staging', Key='public/'+s3_object_key)
