d9bb22508792d668ca80213a68b38bb10167e3d0
Wagner Caetano <wagnercaetano01@gmail.com>
2023-05-28 16:23:18 -0300

    function

--- src/assets/SVGs/ClockImage.js
+++ src/assets/SVGs/ClockImage.js
@@ -1,12 +1,5 @@
-import * as React from "react"
-import Svg, {
-  SvgProps,
-  Path,
-  Defs,
-  Pattern,
-  Use,
-  Image,
-} from "react-native-svg"
+import * as React from "react";
+import Svg, { SvgProps, Path, Defs, Pattern, Use, Image } from "react-native-svg";
 const ClockImage = (props: SvgProps) => (
   <Svg
     xmlns="http://www.w3.org/2000/svg"
@@ -18,12 +11,7 @@ const ClockImage = (props: SvgProps) => (
   >
     <Path fill="url(#a)" d="M0 0h243v243H0z" />
     <Defs>
-      <Pattern
-        id="a"
-        width={1}
-        height={1}
-        patternContentUnits="objectBoundingBox"
-      >
+      <Pattern id="a" width={1} height={1} patternContentUnits="objectBoundingBox">
         <Use xlinkHref="#b" transform="scale(.00098)" />
       </Pattern>
       <Image
@@ -34,5 +22,5 @@ const ClockImage = (props: SvgProps) => (
       />
     </Defs>
   </Svg>
-)
-export default ClockImage
+);
+export default ClockImage;
