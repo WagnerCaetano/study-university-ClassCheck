b6eb3bbb3c0ad3df2432c77890f3965e0458b7ba
wagnercaetano <wagnercaetano01@gmail.com>
2023-06-14 20:06:05 -0300

    ajustando container

--- src/global/container/container.ts
+++ src/global/container/container.ts
@@ -3,17 +3,11 @@ import styled from 'styled-components/native';
 export const Container = styled.View`
     flex: 1;
     display: flex;
-    width: ${(props) => (props.theme?.width ? props.theme?.width : '100%')};
-    flex-direction: ${(props) =>
-        props.theme?.direction ? props.theme?.direction : 'column'};
-    height: ${(props) =>
-        props.theme?.height ? props.theme?.height : 'fit-content'};
-    align-self: ${(props) =>
-        props.theme?.align ? props.theme?.align : 'center'};
-    justify-self: ${(props) =>
-        props.theme?.justify ? props.theme?.justify : 'center'};
-    padding: ${(props) =>
-        props.theme?.padding ? props.theme?.padding : '1.5em'};
-    background: ${(props) =>
-        props.theme?.background ? props.theme?.background : '#fff'};
+    width: 100%;
+    flex-direction: column;
+    height: fit-content;
+    align-self: center;
+    justify-self: center;
+    padding: 1.5em;
+    background: #fff;
 `;
--- src/screens/Sobre/styles.ts
+++ src/screens/Sobre/styles.ts
@@ -34,37 +34,19 @@ export const Profiles = styled.View`
     margin-bottom: 5%;
 `;
 export const PersonImage = styled.Image`
-<<<<<<< HEAD
-  margin-left: 3%;
-  width: 5em;
-  height: 5em;
-  border-radius: 15vw;
-  object-fit: contain;
-=======
     margin-left: 3%;
     width: 5em;
     height: 5em;
     border-radius: 15vw;
     object-fit: contain;
     border: 2px solid #026a6c;
->>>>>>> 1c2d93906fe13f0deee22c6e803e31086de0bf81
 `;
 export const Separator = styled.View`
-<<<<<<< HEAD
-  align-items: center;
-  margin-top: 10px;
-  margin-left: 5%;
-  margin-right: 5%;
-  width: 90%;
-  height: 1;
-  background-color: #026a6d;
-=======
     align-items: center;
     margin-top: 10px;
     width: 90%;
     height: 1;
     background-color: #026a6d;
->>>>>>> 1c2d93906fe13f0deee22c6e803e31086de0bf81
 `;
 export const ContainerEmail = styled.View`
   align-items: center;
